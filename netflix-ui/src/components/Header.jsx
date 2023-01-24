33c62ba713661b4e1a1d00c29d29bb63965355bd
RamboXD <rnazymxan@gmail.com>
2023-01-24 11:09:38 +0600

    navbar and signOut was done

--- netflix-ui/src/components/Header.jsx
+++ netflix-ui/src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header(props) {
         <img src={logo} alt="logo" />
       </div>
       <button onClick={() => navigate(props.login ? "/login" : "/signup")}>
-        {props.login ? "Log In" : "Sign In"}
+        {props.login ? "Log In" : "Sign Up"}
       </button>
     </StyledHeader>
   );
